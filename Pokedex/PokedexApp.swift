f013f131ff5ae20f0fe698e38281f49061cc2168
Egor <iMrRoll@yandex.ru>
2022-10-17 13:37:35 +0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by piggyback13 on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
